75b8aa3caaa8701785934281fb85498737cf0b67
Sattyam Samania <67833888+SattyamSamania@users.noreply.github.com>
2023-12-20 16:56:08 +0530

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
+export const POST = async (request) => {
+    const { userId, prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = new Prompt({ creator: userId, prompt, tag });
+
+        await newPrompt.save();
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
+    } catch (error) {
+        return new Response("Failed to create a new prompt", { status: 500 });
+    }
 }
-
-export default Feed
